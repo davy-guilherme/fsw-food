a1bd5d94654095d10a19b73b8521c7cb880d4703
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-04-28 12:20:07 -0300

    chore: add initial prisma schema

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useState } from "react";
+
+const About = () => {
+    const [name, setName] = useState();
+
+    return ( <h1>About Page</h1> );
+}
+ 
+export default About;
