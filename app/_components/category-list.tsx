cb1c9acdc468eff258925fcca7cdeb7d9e4f1f75
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-04-30 15:27:33 -0300

    feat: add prisma file

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,10 @@
+const CategoryList = () => {
+    //pegar categorias do banco de dados
+    //renderizar um item para cada categoria
+    return (
+        <>
+        </>
+    );
+}
+ 
+export default CategoryList;
