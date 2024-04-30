386f90c664d497a0ad959bb4f6570641f9bf4393
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-04-30 12:53:42 -0300

    feat: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import Header from './_components/header'
+
 const Home = () => {
   return (
-    <div className="bg-red-500 p-5 mx-6 scale-125">
-      <h1>hello tailwind!</h1>
-    </div>
+    <Header />
   );
 };
 
