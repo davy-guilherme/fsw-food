2ea04bbbb8dcc6518eebe8c50dd8691ac0dc33a4
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-05-01 09:49:19 -0300

    feat: add category item and list components

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,9 +1,16 @@
-const CategoryList = () => {
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
     //pegar categorias do banco de dados
+    const categories = await db.category.findMany({});
     //renderizar um item para cada categoria
     return (
-        <>
-        </>
+        <div className="grid grid-cols-2 gap-3">
+            {categories.map(category => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
     );
 }
  
