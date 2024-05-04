3b47996a5f0a4f3fc2710ccddad89b869890fae8
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-05-04 08:37:26 -0300

    feat: add recommended restaurants page

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -1,20 +1,21 @@
 import { Restaurant } from "@prisma/client";
-import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
+import { BikeIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
-import { Button } from "./ui/button";
 import Link from "next/link";
+import { cn } from "../_lib/utils";
 
 interface RestaurantItemProps {
-    restaurant: Restaurant
+    restaurant: Restaurant;
+    className?: string;
 }
 
-const RestaurantItem = ({restaurant}: RestaurantItemProps) => {
+const RestaurantItem = ({restaurant, className}: RestaurantItemProps) => {
 
     return ( 
         <Link 
             href={`/restaurants/${restaurant.id}`}
-            className="min-w-[266px] max-w-[266px]"
+            className={cn("min-w-[266px] max-w-[266px]", className) }
         >
             <div className="w-full space-y-3">
                 <div className="w-full h-[136px] relative">
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import {db} from './_lib/prisma';
-import Image from 'next/image';
 import CategoryList from './_components/category-list';
 import Header from './_components/header'
 import Search from './_components/search';
@@ -8,6 +7,7 @@ import { Button } from './_components/ui/button';
 import { ChevronRightIcon } from 'lucide-react';
 import PromoBanner from './_components/promo-banner';
 import RestaurantList from './_components/restaurant-list';
+import Link from 'next/link';
 
 const Home = async () => {
 
@@ -65,10 +65,12 @@ const Home = async () => {
       <div className="space-y-4 py-6">
         <div className="px-5 flex items-center justify-between">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Button variant="ghost" className="text-primary p-0 hover:bg-transparent h-fit">
-            Ver Todos
-            <ChevronRightIcon size={16} /> 
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button variant="ghost" className="text-primary p-0 hover:bg-transparent h-fit">
+              Ver Todos
+              <ChevronRightIcon size={16} /> 
+            </Button>
+          </Link>
         </div>
         <RestaurantList />
       </div>
