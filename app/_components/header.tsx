37e8d7ac71615c09282ce34ea2df8a682435c0a7
Davy Batista <davybatista@MacBook-Air-de-Davy.local>
2024-05-05 17:34:57 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,20 @@
+"use client"
+
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { HeartIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, ScrollTextIcon } from "lucide-react";
 import Link from "next/link";
+import  {signIn, signOut, useSession} from 'next-auth/react'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
 
 const Header = () => {
+    const {data, status} = useSession();
+
+    const handleSignOutClick = () => signOut();
+    const handleSignInClick = () => signIn();
+
     return ( 
         <div className="flex justify-between pt-6 px-5">
             <div className="relative h-[30px] w-[100px]">
@@ -11,14 +22,116 @@ const Header = () => {
                     <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
                 </Link>
             </div>
+
+            {/* {
+                data?.user.name ? (
+                    <>
+                        <h1>{data.user.name}</h1>
+                        <Button onClick={() => signOut()}>
+                            Sair
+                        </Button>
+                    </> 
+                ) : (
+                    <Button onClick={() => signIn()}>Login</Button>
+                )
+            } */}
+        
+            <Sheet>
+                <SheetTrigger>
+                    <Button 
+                        size="icon"
+                        variant="outline"
+                        className="border-none bg-transparent"
+                    >
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle className="text-left">Menu</SheetTitle>
+                    </SheetHeader>
+
+                        {
+                            // status == 'authenticated'
+                            data?.user ? (
+                                <>
+                                    <div className="flex justify-between">
+                                        
+                                        <div className="flex items-center gap-3">
+                                            <Avatar>
+                                                <AvatarImage src={data?.user?.image as string | undefined} />
+                                                <AvatarFallback>
+                                                    {data?.user?.name?.split(" ")[0][0]}
+                                                    {data?.user?.name?.split(" ")[1][0]}
+                                                </AvatarFallback>
+                                            </Avatar>
+
+                                            <div>
+                                                <h3 className="font-semibold">{data?.user?.name}</h3>
+                                                <span className="text-muted-foregound text-xs">{data?.user?.email}</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="flex justify-between items-center pt-10">
+                                        <h2 className="font-semibold">Faça seu login</h2>
+                                        <Button size="icon" onClick={handleSignInClick}>
+                                            <LogInIcon />
+                                        </Button>
+                                    </div>
+                                </>
+                            )
+                        }
+
+                        <div className="py-6">
+                            <Separator />
+                        </div>
+
+                        <div className="space-y-2">
+                            <Button variant="ghost" className="w-full justify-start space-x-3 text-sm font-normal rounded-full">
+                                <HomeIcon size={16}/>
+                                <span className="block">Início</span>
+                            </Button>
+
+                            {
+                                data?.user && (
+                                    <>
+                                        <Button variant="ghost" className="w-full justify-start space-x-3 text-sm font-normal rounded-full">
+                                            <ScrollTextIcon size={16}/>
+                                            <span className="block">Meus Pedidos</span>
+                                        </Button>
+
+                                        <Button variant="ghost" className="w-full justify-start space-x-3 text-sm font-normal rounded-full">
+                                            <HeartIcon size={16}/>
+                                            <span className="block">Restaurantes Favoritos</span>
+                                        </Button>
+                                    </>
+                                )
+                            }
+                        </div>
+
+                        <div className="py-6">
+                            <Separator />
+                        </div>
+                        {
+                            data?.user && (
+                                <Button 
+                                    variant="ghost" 
+                                    className="w-full justify-start space-x-3 text-sm font-normal rounded-full"
+                                    onClick={handleSignOutClick}    
+                                >
+                                    <LogOutIcon size={16}/>
+                                    <span className="block">Sair da conta</span>
+                                </Button>
+                            )
+                        }
+                        
+                </SheetContent>
+            </Sheet>
             
-            <Button 
-                size="icon"
-                variant="outline"
-                className="border-none bg-transparent"
-            >
-                <MenuIcon />
-            </Button>
         </div>
     );
 }
